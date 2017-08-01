506613dd9d6b585455a6f8bd85a4a2499e8d5c4a
jpotts8 <jp.z.potts@gmail.com>
2017-08-01 15:39:28 -0500

    Added choices through counseling

---
+++ Aiden.py
@@ -0,0 +1,80 @@
+ import time
+
+ dream='''
+ You are in a dark roon by yourself, no windows, just you and your breath.
+ Suddenly, a gorgeous boy starts strutting towards you with a smirk in his face
+ As he gets closer you realize its your ex, things didn't end well with him and you being the only
+ gay couple in school, so you two broke it off and he moved away...things ended on a sad note.
+ He walks closer and closer, you start to walk towards him as well, soon turning into a light run
+ towards him, which goes into a sprint to hug him, but it seems as if the more you run the farther
+ away you are form him. Suddenly a huge door opens, and out walks a ginormous monster coming right
+ for you, you take one look back at your ex as he solemly walks away, the monster rushes towards you
+ malice in its eyes, you try to run away but it gets closer and closer, soon its too late but as
+ soon as he has you in his grip, you hear your alarm sounding. it is time to wake up.
+ '''
+
+ wake up='''
+ You wake up in a cold sweat, realizing the monster in your dream was your bully Flash Bunswick
+ You sigh as you get up and turn off your alarm.
+ On your way to the bathroom you see your mom,
+"Are you ok sweetie you're all sweaty?!"
+You wipe your forehead and feel a stream of sweatbeads on the back of your hand
+"Yeah Mom, I'm fine, I was working out for a bit before i had to get up"
+"Oh..ok hun"
+You hastily make your way to the bathroom so your mom doesn't ask anymore questions, as if you
+would tell her about your bully, she already has your dad to worry about
+him being in the army and all, it would kill you to add onto that.
+'''
+School='''
+You finally manage to satisfy your mom enough to get out of the house without questioning you more
+You get to school and go to your locker to get your books for class, everything seems to be going
+fine until you suddenly her a *slam* on your locker, and sure enough on the other side is your
+bully, giving you an evil grin
+"What's up Ugly?"
+(side note Aiden is not in the least bit ugly)
+You can either relaliate against him or let his comment about your appearance go, which will
+you choose?
+'''
+
+Retaliate='''
+You retaliate against the bully:
+"Y'know? You have your ENTIRE life to be a jerk, why not take a day off?"
+Flash steps back with his eyes buldging like he's reevaluating his life decisions up to this point
+"Y-you're right, this is pretty tiring, I think I will take a day off!"
+You sigh in relief
+"Good" you say as you try to reopen your locker to get your books
+"Sike!" Flash says as he shoves you down to the floor and laughs at you
+"Nathanial Bunswick!"
+Everyone turns around to see the principal staring at Flash aka Nathanial Bunswick
+with a summoning finger straight to his office
+"Shoot" says Flash he looks at you angrily "This is your fault! I'll make you pay!"
+he whispers before walking to the principal's office
+They pricipal and Flash walk into the office and close the door,
+"I didn't even do anything *insert badword here*!!"
+You say getting up and dusting yourself office
+The Principal whips back open the office door,"Aiden!!, you get in here as well!
+You know swearing isn't allowed in school!" the pricipal stops yelling and thinks for a second.
+"Looks like you boys could use some time to discuss your problems with eachother, both of you
+meet with the school counselor after school starting today for the rest of the week and next week."
+Flash complains and so do you, but it quickly stops when the principal offers detentions instead.
+"Great" you both think "A whole two weeks with this idiot".
+'''
+Let it go='''
+You decide to let Flash's comment go and ignore him:
+Flash: "Hey, what's your problem? Nothing to say? Cat got your tongue?"
+Flash-"...Whatever! I don't have time for this anyway!"
+Flash walks away, slaming your books down on the way
+You- "What an idiot" you say picking up your books, suddenly a boy comes to your vision, he
+starts helping you pick up your books, you haven't seen him around, "He must e new" you think.
+You can either let him help you or shoo him away Which will you choose?
+'''
+Counseling='''
+You head to the counselor's office after you are done with your classes, and sit on the couch
+ waiting for Flash and Mrs.Andrews, the school counselor. Afetr a few minutes Flash comes in,
+sees you and rolls his eyes, throwing his bag next to the leather couch and plopping down
+on the cushion farthest from you, before you can either say anything to eachother
+Mrs.Andrews walks in cheerily
+Mrs.Andrews-"Hiiiii boysssss!! How are you today?"
+Neither of you respond
+Mrs.Andrews-"Well in that case I'm doing fine thanks for asking, let's dive into why we are here
+it seems like neither if you can get along "
