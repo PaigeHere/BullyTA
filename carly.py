cf2bb99874f02bf1e2d3bde04f611cb997f0380b
lucykaskel <kaskeltoo@gmail.com>
2017-08-01 14:19:59 -0500

    Beginning of code.

---
+++ carly.py
@@ -0,0 +1,200 @@
+cSocialMedia = """
+    You took a few pictures today and you were TOTALLY feeling yourself! You're debating between the selfie, where your highlighter
+    and eyebrows look AMAZING! You're literally glowing in the picture-which is why you love it so much! But, you also love the one your
+    friend took of you. Your back is facing the camera, and your head is turned to look back at the camera. Your bod looks killer, and
+    it's totally worth showing off. After like THREE hours of cancelling out pictures, your still stuck between these two. Which do you pick?
+"""
+
+cFake = """
+    Finally you decided on the makeup picture! You know you chose well when your phone starts blowing up with likes and comments. You even
+    get a few snapchats from some guys...go you! While scrolling through your comments you come across a rude one from a girl at school
+    named Ali. You two aren't really friends, you've only spoken a few times. The comment says, 'Looking fake af. Take her swimming on the first
+    date boys!' It's rude, and hurts your feelings a little bit. You've never really had a mean comment on any of your pictures before so, you aren't
+    quite sure how to react. You could either stalk some of her pictures on instagram and comment back something rude to her, or just ignore the comment
+    and delete it.
+"""
+
+cShaming = """
+    Yes! The body picture is amazing and you're getting tons of likes and comments! A few guys snapchat you, and you feel great about it! You're self
+    confidence is through the roof! While scrolling through the comments, you see one rude one from a girl named Ali who goes to your school. It says, 'Keep
+    your legs closed, slut.' You don't really understand where she's coming from, you're not the type to be with tons and tons of guys, and your not even
+    close friends with her- so it's none of her business! Regardless, it hurts your feelings and drops your self-esteem quite a bit. You could either ignore
+    the comment and delete it, or respond something snarky- possibly starting a fight with her.
+"""
+
+cFacebook = """
+    You find your inner chill and ignore her comment. Eventually you stop thinking and caring about it, so you clearly made the right decision not to fight her
+    or start up unnecessary drama. A few days after posting the picture, you see a facebook request from someone unfamiliar. His profile picture is super cute,
+    and he looks about your age, 17. You have about 20 mutual friends, so you assume that he goes to a school in your district. Do you accept his friend request
+    or deny it?
+"""
+
+cSchool = """
+    Ali never responded to the comments you left, which made you feel powerful, like you won! You regained your confidence a bit, but still feel kind of upset
+    about the rude things she said. Then again, you said equally rude things to her after. The next day at school while you swap a few books from your backpack
+    to your locker, Ali and her group of friends walk by. You instantly want to shrivel up into a ball, and regret saying rude things to her. You should've just
+    ignored it! She stops near you, and you hear her and her friends mumble some hurtful things about you and call you some mean names. They give you dirty looks
+    and then walk away, but you feel like crying. Do you go and tell the dean, or try and get your friends to turn against Ali and her friends so you have some
+    people to back you up?
+"""
+
+cDate = """
+    After accepting his friend request, the boy, Alec, starts messaging you. You two both hit it off instantly! You find yourself sharing personal information
+    with him about your family life and some other drama. He shares things to, and you feel so connected and confident in trusting him! He's super sweet and cute,
+    and you can't believe you've found someone who you can connect to so well! One day while chatting, he says he has a question for you. Unlike his normal, chattery
+    self, you can tell he's feeling awkard and nervous. Even though it's communicating over a screen, you just have this feeling. Then you find out your hunch was because
+    he was going to ask you on a date! OMG! Do you say yes or no? You're conflicted because you've never met him in person, although he says he goes to a neighboring
+    school, but you also have such a deep connection how could you say no? So, what do you do?
+"""
+
+cSeeAgain = """
+    You click deny with little hesistation. It's better to stay safe and ignore weirdo's that might not be real! The next day, you're out for breakfast
+    with your best friend, Mia. While finishing up your breakfast, you see a familiar face walk in. He makes eye contact with you, and you realize it's the guy
+    you denied on Facebook. You feel a little awkward and look away, until one of his friends approaches you. He asks, 'How was my boy Alec? Heard you're pretty
+    crazy!' You give him a confused look and he turns back to his friends laughing. He mumbles 'slut', under his breath while walking away and you are just so confused!
+     Days later, you hear a rumor that you had hung out with Alec when you haven't even spoken to him! Everyone's talking about you, and you have no clue how to fix
+    this problem. Do you ask your mom for some help and confide in her, or confront Alec for telling his friends lies?
+"""
+
+cHumiliated = """
+    Oh my god! You said yes to Alec, and he asks to meet at a frozen yogurt place nearby tomorrow night! The next day soon arrives, and you're SO excited!
+    Once you get there, he's nowhere to be found. You send him a facebook message asking where he is, and he responds with 'Almost there, traffic-sorry!'.
+    You wait about five more minutes before seeing Ali and her friends walk in. Ugh! You think you probably look like a loser waiting here alone, if only
+    they knew a cute guy was coming to meet you! Ali walks over to your table with a smirk, and drops her phone in front of you- revealing all the texts you
+    shared with Alec. At first you're confused, and Ali can tell. 'Don't be an idiot. Alec isn't real, Carly.' You feel like crying. How could she do this?
+    'By the way, I never knew your dad left your mom for some girl five years older than you. Now everyone will know everything you said to him- I mean me!' She
+    laughs, and her friend mumbles something about you being an idiot. She knows EVERYTHING! You feel lost, sad and angry, but don't know what to do. Do you
+    tell your mom in hopes she could possibly help, or ignore them and try to move past this?
+"""
+
+cReveal = """
+    You were always told- stranger danger. Even though you trusted Alec, you felt uncomfortable meeting him alone at night. You suggest a more public meeting, and
+    he doesn't respond. He doesn't message you for days, and finally you get one four days later. 'Don't talk to me again.' You're sad, losing a good
+    friend, but clearly he's a little over-sensitive if he was this pissed about postponing a date for a group event. A day later, you're scrolling through
+    facebook and you see he posted something on his timeline. It's screenshots of your messages with Alec! How could he do this? They're so personal,
+    and so many people are commenting 'Haha' and 'That's a messed up life', or versions of those comments. You start to cry, not understanding why
+    he would do this to you. Do you tell the dean hoping that he could do something about this cyber attack and possibly talk to the super intendent? Or
+    do you ask your mom to help you?
+"""
+
+def cIntro():
+    print(cSocialMedia)
+    done = False
+    while not done:
+        user_input = str (input("Type 'makeup' if it's the glowing selfie, or 'body' if it's the picture of your great body: "))
+        if user_input == "makeup":
+            print(cFake)
+            cMakeup()
+            done = True
+        elif user_input == "body":
+            print(cShaming)
+            cBody()
+            done = True
+        else:
+            print("Please type 'makeup' or 'selfie'");
+
+def cMakeup():
+    done = False
+    while not done:
+        user_input = str (input("Type 'ignore' if you want to take the high road and be mature, or 'comment' if you want to comment something rude on her picture: "))
+        if user_input == "ignore":
+            print(cFacebook)
+            cRequest()
+            done = True
+        elif user_input == "comment":
+            print(cSchool)
+            cDirtyLooks()
+            done = True
+        else:
+            print("Please type 'ignore' or 'comment'");
+
+def cBody():
+    done = False
+    while not done:
+        user_input = str (input("Type 'ignore' if you want to take the high road and be mature, or 'fight' if you want to respond something rude and start a fight: "))
+        if user_input == "ignore":
+            print(cFacebook)
+            cRequest()
+            done = True
+        elif user_input == "fight":
+            print(cSchool)
+            cDirtyLooks()
+            done = True
+        else:
+            print("Please type 'ignore' or 'fight'");
+
+def cRequest():
+    done = False
+    while not done:
+        user_input = str (input("Type 'accept' if you want to become facebook friends with him, or 'deny' if not: "))
+        if user_input == "accept":
+            print(cDate)
+            cAskOut()
+            done = True
+        elif user_input == "deny":
+            print(cSeeAgain)
+            cChoice()
+            done = True
+        else:
+            print("Please type 'ignore' or 'fight'");
+
+def cSchool():
+    done = False
+    while not done:
+        user_input = str (input("Type 'dean' if you want to tell the dean, or 'friends' if you want to turn your friends against Ali and her squad: "))
+        if user_input == "dean":
+            print(cDean)
+            cEndGame()
+            done = True
+        elif user_input == "friends":
+            print(cFire)
+            cEndGame()
+            done = True
+        else:
+            print("Please type 'dean' or 'friends'");
+
+
+def cChoice():
+    done = False
+    while not done:
+        user_input = str (input("Type 'mom' if you want to ask your mom to help, or 'confront' if you want to handle this on your own with Alec. : "))
+        if user_input == "mom":
+            print(cMom)
+            cEndGame()
+            done = True
+        elif user_input == "confront":
+            print(cApology)
+            cEndGame()
+            done = True
+        else:
+            print("Please type 'mom' or 'confront'");
+
+def cAskOut():
+    done = False
+    while not done:
+        user_input = str (input("Type 'yes' if you want to go out with him, or 'no' if not: "))
+        if user_input == "yes":
+            print(cCatfish)
+            cHumiliated()
+            done = True
+        elif user_input == "no":
+            print(cReveal)
+            cPosted()
+            done = True
+        else:
+            print("Please type 'mom' or 'confront'");
+
+def cHumiliated():
+    done = False
+    while not done:
+        user_input = str (input("Type 'mom' if you want to ask your mom to help, or 'confront' if you want to handle this on your own with Alec. : "))
+        if user_input == "mom":
+            print(cMom)
+            cEndGame()
+            done = True
+        elif user_input == "confront":
+            print(cApology)
+            cEndGame()
+            done = True
+        else:
+            print("Please type 'mom' or 'confront'");
