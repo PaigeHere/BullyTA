cf670781fdfc4de6c41fbdec9871122273a560ce
Paige <cellomaster107@gmail.com>
2017-08-03 11:06:25 -0500

    I added the last path, so all paths should be completed. It still requires testing and editing.

--- Caleb.py
+++ Caleb.py
@@ -3,7 +3,7 @@ import time
 start = '''
 WELCOME to Stand Up or Stand Down- Presented by TPMLJ.As you sit down to play this game, center yourself, take a deep breath,
 and wash away all preconceived ideas you have about bullying.  Take a chance and explore the different characters’ lives
- in these different stories. Please choose your character: caleb, logan, aiden, carly
+ in these different stories. Please choose your character: Caleb, logan, aiden, carly
 '''
 bIntro = '''
 You are on your way to tryouts. It’s a new season, and you and your closest friends are the team captains.
@@ -11,6 +11,12 @@ As team captains you run the tryout practices to select the best new member for 
 As you look at him yourself, your friends laugh and joke about how unqualified he must be. “Can he even hold the ball with those noodle arms?”, they say.
 You aren’t amused but laugh anyways to get them off your back. Your friends stop laughing and turn to you. “Yo, Caleb. We should totally give him a hard time.
 Why don’t you mess with him. Come on. It would  be funny!”
+'''
+bFriends = '''
+You don’t feel good about that idea. Your friends look at you expectantly, but you don’t want to give in to their pressure. “Nah, I don’t think I want to.
+That doesn’t really seem right.”  They look at you still smiling, “Come on, Caleb. You can’t be serious! It’s just a little fun! It’s no big deal. What do you say?”
+They really want you to do this. You think of your options: You could just mess around with the kid. You really don’t want to disappoint your friends, but at the same time.
+You’re not sure if it would be the right thing to do. Is it worth it? Your friends wait for a reply, “So buddy. What d’ya say?”, one of them asks.
 '''
 bPractice = '''
 You decide to give into your friends. “Yea, sure. I’ll mess up the kid. This’ll be hilarious”, you lie to them. As you watch the boy doing the different stations
@@ -42,7 +48,7 @@ To my surprise all the other captains exchanged glances, got up, and followed hi
 He says. “Let’s go out for food.” Nathan, you and all the boys go to the nearest fast food place and stuff yourselves. You decide to welcome all the tryouts to the team.
 '''
 
-dAgainst = '''
+bAgainst = '''
 After School the next day, you go to practice. All your new teammates joke with you in breaks, and on the field they listen to you. Meanwhile, the other captains have been shooting you glares.
 They repeatedly make mean comments about you and laugh while you’re helping the new teammates. You’re annoyed, but you don’t want to make the situation worse than it is, so you don’t say anything
  to them. After practice ends you go into the locker room to shower. You can hear your mom’s voice in your head, “Caleb if you try to get into my car like a sweaty mess then you’re walking home.”
@@ -99,46 +105,52 @@ you respond. “Actually yes. I was kicked off the team. Thanks for nothing!” 
 good idea. All you did was hurt someone for fun and then lose. Without practice, you grow distant from your “friends”. You realize more and more what bullies they are, and you are
 ashamed that you were one yourself. “Never again…” you think. Next time I will Stand Up against what is wrong. I will Stand Down to those I hurt.  The End
 '''
+bFlip='''
+“Sorry. I just saw you trying so hard out there, I was disappointed when you weren’t the first back”. He looked confused, “But I came back after others because they didn’t actually
+finish their station and left early from it.”  He has a good point, “Okay, yea, I get. Sorry. You shouldn’t have ran; I should have made some of the other boys run. Can we forget this
+happened? It was my bad. I was a little on edge for the first day of practice.” He squints at you, trying to see the lie, but you truly are sorry and don’t want to hurt him. “Okay”,
+he says, “Just don’t make me run for no reason again. Deal?” You’re relieved by his understanding, “Deal.” You shake hands and go home. The next day at practice, Coach calls you over.
+“Hey Caleb. I heard from the other captains that you weren’t treating Nathan properly. I heard you were even harassing him after practice. I’m sorry but that is not within our team contract.
+I’m going to have to cut you from the team.” You can’t believe what you heard. Everything goes fuzzy and numb. You walk over to the bench to pick up your bag. The captains are sneering
+at you and whispering. As you leave you hear Coach yell after you, “I’m sorry Caleb! Come for some of the games!” As you walk home you think about this whole arch of events. You grow
+angry towards your “friends”. You have learned a lot from this. Nothing good can come out of hurting someone for the enjoyment of others. It will come back to hurt you, and you now
+know this. Time moves on and eventually the other captains are found out by the coach and cut from the team for harassment towards the players. After you Apologized to Nathan, you
+both grow closer as friends. You have learned and lost, but also gained a good friend. Now you know to Stand Up for what is right and Stand Down to those you have done wrong. The End.
+'''
 
-print(start)
-
-done = False
-while not done:
-    character = input("Type the character's name! ")
-    if character == "Caleb":
-        print(bIntro)
-        done = True
-
-done = False
-while not done:
-    user_input = input("What do you do? Do you give into the pressure? Type Yes or No: ")
-    if user_input == "Yes":
-        print(bPractice)
-        bViolence()
-        done= True
-    elif user_input == "No":
-        print(poo)
-        sleep.time(5)
-        done= True
-    else:
-        print("Please type Yes or No" )
-
-Def bViolence()
+def bApologize():
+    print(bAgainst)
     done = False
     while not done:
-        user_input = input("What will you do? Type Punch or Humiliate: ")
-        if user_input == "Punch":
-            print(bfists)
-            bPunch()
+        user_input = input("Type Report or Forgive: ")
+        if user_input == "Report":
+            print(bReport)
+            time.sleep(10)
             done = True
-        elif user_input == "Humiliate":
-            print(bRunning)
-            bHumiliate()
+        elif user_input == "Forgive":
+            print(bForgive)
+            time.sleep(10)
             done = True
         else:
-            print("Please type Punch or Humiliate.")
+            print("Please type Report or Forgive.")
 
-Def bPunch()
+
+def bHumiliate():
+    done = False
+    while not done:
+        user_input= input("What do you do? Type Apologize or Punch: ")
+        if user_input == "Apologize":
+            print(bFlip)
+            time.sleep(8)
+            done = True
+        elif user_input == "Punch":
+            print(bHit)
+            time.sleep(8)
+            done = True
+        else:
+            print("Please type Apologize or Punch." )
+
+def bPunch():
     done = False
     while not done:
         user_input = input("What will you do? Type Apologize or Ignore: ")
@@ -148,51 +160,58 @@ Def bPunch()
             done = True
         elif user_input == "Ignore":
             print(bIgnore)
-            sleep.time(8)
+            time.sleep(8)
             done = True
         else:
-            print("Please type Apologize or Ignore: ")
+            print("Please type Apologize or Ignore.")
 
 
-Def bHumiliate()
+def bViolence():
     done = False
     while not done:
-        user_input= input("What do you do? Type Apologize or Punch: ")
-        if user_input == "Apologize":
-            print()
-            bFrenemy()
-            done = True
-        elif user_input == "Punch":
-            print(bHit)
-            sleep.time(8)
-            done = True
-        else:
-            print("Please type )
-
-Def bFrenemy()
-    done = False
-    while not done:
-        user_input = input("Type Punch or Ignore: ")
+        user_input = input("What will you do? Type Punch or Humiliate: ")
         if user_input == "Punch":
-            print(bHit)
-            sleep.time(8)
-        elif user_input == "Ignore":
-            print()
-            sleep.time(8)
+            print(bFists)
+            bPunch()
+            done = True
+        elif user_input == "Humiliate":
+            print(bRunning)
+            bHumiliate()
+            done = True
         else:
-            print("Please type Punch or Ignore: ")
-Def bApologize
-    print(dAgainst)
+            print("Please type Punch or Humiliate.")
+def bDouble():
     done = False
     while not done:
-        user_input = input("Type Report or Forgive: ")
-        if user_input = Report:
-            print(bReport)
-            sleep.time(10)
-            done = True
-        elif user_input = Forgive:
-            print(bForgive)
-            sleep.time(10)
+        user_input = input("What will you do? Type Accept or Refuse: ")
+        if user_input == "Accept":
+            print(bPractice)
+            bViolence()
+            doen = True
+        elif user_input == "Refuse":
+            bApologize()
+            time.sleep(8)
             done = True
         else:
-            print("Please type Report or Forgive: ")
+            print("Please type Accept or Refuse.")
+print(start)
+
+done = False
+while not done:
+    character = input("Type the character's name! ")
+    if character == "Caleb":
+        print(bIntro)
+        done = True
+done = False
+while not done:
+    user_input = input("What do you do? Do you give into the pressure? Type Yes or No: ")
+    if user_input == "Yes":
+        print(bPractice)
+        bViolence()
+        done= True
+    elif user_input == "No":
+        print(bFriends)
+        dDouble()
+        done= True
+    else:
+        print("Please type Yes or No" )
